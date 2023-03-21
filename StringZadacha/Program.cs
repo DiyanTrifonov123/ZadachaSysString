f3b816168e9eeb8557463dafae650447ea500911
DiyanTrifonov123 <diyantrifonov5@gmail.com>
2023-03-21 09:40:03 +0200

    Second commit

--- StringZadacha/Program.cs
+++ StringZadacha/Program.cs
@@ -14,23 +14,65 @@ namespace StringZadacha
             string[] str = Console.ReadLine().Split().ToArray();
             string str1 = str[0];
             string str2 = str[1];
-            int result = CharCodes(str1, str2);
-            Console.WriteLine($"Сборът на string-овете е: {result}");
+            //int sum = 0;
+            int[] sumsCharCode = new int[Math.Max(str1.Length, str2.Length)];
+            sumsCharCode = MultiplyAllChars(str1, str2,sumsCharCode);
+            int finalResult = SumAllCodes(sumsCharCode);
+
+            //int length = Math.Min(str1.Length, str2.Length);
+
+            //for (int i = 0; i < length; i++)
+            //{
+            //    sumsCharCode[i] = (int)str1[i] * (int)str2[i];
+            //}
+
+            //for (int j = length; j < sumsCharCode.Length; j++)
+            //{
+            //    if (str1.Length > str2.Length)
+            //    {
+            //        sumsCharCode[j] = (int)str1[j];
+            //    }
+            //    else
+            //    {
+            //        sumsCharCode[j] = (int)str2[j];
+            //    }
+            //}
+
+            Console.WriteLine($"Умножението на char-овете: {string.Join(", ", sumsCharCode)}");
+            Console.WriteLine($"Сборът на char-овете е {finalResult}");
         }
 
-        private static int CharCodes(string str1, string str2)
+        private static int SumAllCodes(int[] sumsCharCode)
         {
-            int length = Math.Max(str1.Length, str2.Length);
-            int sum = 0;
+            int result = 0;
+            for (int i = 0; i < sumsCharCode.Length; i++)
+            {
+                result = result + sumsCharCode[i];
+            }
+            return result;
+        }
+
+        private static int[] MultiplyAllChars(string str1, string str2, int[] sumsCharCode)
+        {
+            int length = Math.Min(str1.Length, str2.Length);
 
             for (int i = 0; i < length; i++)
             {
-                int charCode1 = i < str1.Length ? (int)str1[i] : 1;
-                int charCode2 = i < str2.Length ? (int)str2[i] : 1;
-                sum += charCode1 * charCode2;
+                sumsCharCode[i] = (int)str1[i] * (int)str2[i];
             }
 
-            return sum;
+            for (int j = length; j < sumsCharCode.Length; j++)
+            {
+                if (str1.Length > str2.Length)
+                {
+                    sumsCharCode[j] = (int)str1[j];
+                }
+                else
+                {
+                    sumsCharCode[j] = (int)str2[j];
+                }
+            }
+            return sumsCharCode;
         }
     }
 }
